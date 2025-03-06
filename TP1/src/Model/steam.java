37e1eb4d7e54bc7287b0670c07aa2afc74234e14
lucaslopes0603 <lucaslopes0603@outlook.com>
2025-03-06 17:42:15 -0300

    opopo

--- TP1/src/Model/steam.java
+++ TP1/src/Model/steam.java
@@ -1,8 +1,5 @@
 package Model;
 
-import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
@@ -10,94 +7,62 @@ public class steam {
     private int appid;
     private String name;
     private LocalDate release_date;
-    private boolean english;
-    private String developer;
-    private String publisher;
-    private ArrayList<String> plataforms;
-    private int required_age;
-    private String categories;
+    private ArrayList<String> platforms;
     private String genres;
-    private String steamspy_tags;
-    private String achievements;
-    private int positive_ratings;
-    private int negative_ratings;
-    private int average_playtime;
-    private int median_playtime;
-    private String owners;
-    private Double price;
 
-    public steam(int appid, String name, LocalDate release_date, boolean english, String developer, String publisher,
-            ArrayList <String> plataforms,int required_age, String categories, String genres, String steamspy_tags,String achievements,
-            int positive_ratings,int negative_ratings,int average_playtime,int median_playtime,String owners,Double price) {
-
+    public steam(int appid, String name, LocalDate release_date, ArrayList<String> platforms, String genres) {
         this.appid = appid;
         this.name = name;
         this.release_date = release_date;
-        this.english = english;
-        this.developer = developer;
-        this.publisher = publisher;
-        this.plataforms = plataforms;
-        this.required_age = required_age;
-        this.categories = categories;
+        this.platforms = platforms;
         this.genres = genres;
-        this.steamspy_tags = steamspy_tags;
-        this.achievements = achievements;
-        this.positive_ratings = positive_ratings;
-        this.negative_ratings = negative_ratings;
-        this.average_playtime = average_playtime;  
-        this.median_playtime = median_playtime;
-        this.owners = owners;
-        this.price = price;
     }
 
     public steam() {
-
         this.appid = -1;
         this.name = "";
         this.release_date = null;
-        this.english = false;
-        this.developer = "";
-        this.publisher = "";
-        this.plataforms = null;
-        this.required_age = -1;
-        this.categories = "";
+        this.platforms = new ArrayList<>();
         this.genres = "";
-        this.steamspy_tags = "";
-        this.achievements = "";
-        this.positive_ratings = -1;
-        this.negative_ratings = -1;
-        this.average_playtime = -1; 
-        this.median_playtime = -1;
-        this.owners = "";
-        this.price = 0.0;   
     }
 
-    public byte[] toByteArray() throws IOException {
-
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-
-        DataOutputStream dataOutputStream = new DataOutputStream(byteArrayOutputStream);
-
-        dataOutputStream.writeInt(appid);
-        dataOutputStream.writeUTF(name);
-        dataOutputStream.writeUTF("");
-        dataOutputStream.writeBoolean(english);
-        dataOutputStream.writeUTF(developer);
-        dataOutputStream.writeUTF(publisher);
-        dataOutputStream.writeUTF("");
-        dataOutputStream.writeInt(required_age);
-        dataOutputStream.writeUTF(categories);
-        dataOutputStream.writeUTF(genres);
-        dataOutputStream.writeUTF(steamspy_tags);
-        dataOutputStream.writeUTF(achievements);
-        dataOutputStream.writeInt(positive_ratings);
-        dataOutputStream.writeInt(negative_ratings);
-        dataOutputStream.writeInt(average_playtime);
-        dataOutputStream.writeInt(median_playtime);
-        dataOutputStream.writeUTF(owners);
-        dataOutputStream.writeDouble(price);
-
-        return byteArrayOutputStream.toByteArray();
+    public int getAppid() {
+        return appid;
     }
 
+    public void setAppid(int appid) {
+        this.appid = appid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getReleaseDate() {
+        return release_date;
+    }
+
+    public void setReleaseDate(LocalDate release_date) {
+        this.release_date = release_date;
+    }
+
+    public ArrayList<String> getPlatforms() {
+        return platforms;
+    }
+
+    public void setPlatforms(ArrayList<String> platforms) {
+        this.platforms = platforms;
+    }
+
+    public String getGenres() {
+        return genres;
+    }
+
+    public void setGenres(String genres) {
+        this.genres = genres;
+    }
 }
