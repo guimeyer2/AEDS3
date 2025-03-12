01648380d95cb5e64c704fbebc8fd8c94e5ced73
Guilherme <guimeygui@gmail.com>
2025-03-12 15:55:01 -0300

    Comentários

--- TP1/src/Model/steam.java
+++ TP1/src/Model/steam.java
@@ -5,11 +5,12 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 // Tirar as colunas que nao estao sendo utilizadas e criar o objeto pra ser utilizado no Actions.
 public class steam {
-    private int appid;
-    private String name;
-    private LocalDate release_date;
-    private ArrayList<String> platforms;
-    private String genres;
+    private int appid; //inteiro 
+    private String name; // string de tamanho variado
+    private LocalDate release_date; //data
+    private ArrayList<String> platforms; // lista com separador
+    private String genres; // string de tamanho variado
+    // launchbefore2010 - string de tamanho fixo
 
     public steam(int appid, String name, LocalDate release_date, ArrayList<String> platforms, String genres) {
         this.appid = appid;
